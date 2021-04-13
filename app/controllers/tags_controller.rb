970d758fb6b29fb56168e691d4f3e487e8207ea0
Joaquin Casanova <joaco.c91@gmail.com>
2021-04-12 23:40:31 -0300

    added filter by tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,11 @@
 class TagsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :index ]
   def index
-    @tags = Tag.all
+    if params[:query].present?
+      sql_query = "name ILIKE :query"
+      @tasks = Task.joins(:tags).where(sql_query, query: "%#{params[:query]}%")
+    else
+      @tasks = Task.all
+    end
   end
 end
